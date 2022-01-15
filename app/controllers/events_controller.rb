32a9997f473c96b1e2349fef6a964125a41addf3
ropajoal <ropajoal@hotmail.es>
2022-01-14 23:03:47 -0400

    Events setup

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,27 @@
 class EventsController < ApplicationController
+  before_action :authenticate_user! , only: [:new, :create]
+  def new
+    @event = Event.new
+  end
+  def create
+    @event = current_user.created_events.build(event_params)
+    #@post = Post.new(post_params)
+    #@post.user_id = current_user.id
+    if @event.save
+      redirect_to new_event_path
+    else
+      puts @event.errors.full_messages
+      render :new
+    end
+  end
   def index
+    @events = Event.all
+  end
+  def show
+    @event = Event.find(params[:id])
+  end
+  private
+  def event_params
+    params.require(:event).permit(:event_date, :title)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  get 'events/index'
+  devise_for :users
+  get '/users/:id', to: 'users#show'
+  resources :events, only: [:index, :show, :new, :create]
+  root 'events#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
