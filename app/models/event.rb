1c05524c7a4c28ac397cb3c1c3fc546968486881
ropajoal <ropajoal@hotmail.es>
2022-01-17 21:54:12 -0400

    Past Upcomming Event Index

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,4 +3,11 @@ class Event < ApplicationRecord
   has_many :attendees, through: :attendee_events 
 
   belongs_to :creator, class_name: "User"
+
+  def self.past
+    Event.where("event_date < ?",Date.today)
+  end
+  def self.upcomming
+    Event.where("event_date >= ?",Date.today)
+  end
 end
